cef75b41e698b671c25696f8ea102b647f317237
Калистый Никита <prostonikita8848@gmail.com>
2022-03-06 01:08:45 +0200

    Task 1/25

---
+++ app/src/main/java/com/application/kgtuapp/Classes/ScheduleDay.kt
@@ -0,0 +1,15 @@
+package com.application.kgtuapp.Classes
+
+class ScheduleDay(id:Int, studyGroup: String, studyWeekNumber: Int) {
+    override fun equals(other: Any?): Boolean {
+        return super.equals(other)
+    }
+
+    override fun hashCode(): Int {
+        return super.hashCode()
+    }
+
+    override fun toString(): String {
+        return super.toString()
+    }
+}
