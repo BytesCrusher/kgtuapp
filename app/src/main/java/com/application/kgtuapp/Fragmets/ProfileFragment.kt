03233639d866ae980badd15f2d49ae20f25499d9
Калистый Никита <prostonikita8848@gmail.com>
2022-03-15 00:30:33 +0200

    new branch

--- app/src/main/java/com/application/kgtuapp/Fragmets/ProfileFragment.kt
+++ app/src/main/java/com/application/kgtuapp/Fragmets/ProfileFragment.kt
@@ -6,14 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.application.kgtuapp.R
+import com.application.kgtuapp.databinding.FragmentProfileBinding
+import com.application.kgtuapp.databinding.FragmentScheduleBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+
+        /*MaterialAlertDialogBuilder(context)
+        Да? Пизда!
+            // Add customization options here
+            .show()*/
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
