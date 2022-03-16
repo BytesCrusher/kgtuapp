36189c959bc08e8bd5f53586fdfff0aea3fe1ea4
Калистый Никита <prostonikita8848@gmail.com>
2022-03-16 21:21:27 +0200

    update for Vmain

--- app/src/main/java/com/application/kgtuapp/MainActivity.kt
+++ app/src/main/java/com/application/kgtuapp/MainActivity.kt
@@ -17,8 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
+        //изменние для коммита
 
         fun changeToolbarTitle(newTitle: String){
             binding.mainToolBar.setTitle(newTitle)
