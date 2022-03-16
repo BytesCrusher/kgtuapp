d9bbe63256934f4681ad499cd7f5c18b831e10a1
Калистый Никита <prostonikita8848@gmail.com>
2022-03-16 21:10:53 +0200

    bug fix 228

--- app/src/main/java/com/application/kgtuapp/MainActivity.kt
+++ app/src/main/java/com/application/kgtuapp/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     private val dataModel: DataModel by viewModels()
     //коммент
+    //git pull
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
